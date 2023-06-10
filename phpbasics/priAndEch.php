77b46cf7307586124a3eb1db9bfaef0d28b9090e
akashraidev <akashrai3334@gmail.com>
2023-06-10 08:04:27 +0530

    print/echo

--- phpbasics/priAndEch.php
+++ phpbasics/priAndEch.php
@@ -5,7 +5,9 @@
 
     <?php
     echo "Akash" . "Rai <br>"; //Dot use for concatinate
-    echo "Akash", "Ray";
+    echo "Akash", "Ray <br>";
+    print "print is slower than echo  <br>";
+    print "Akash" . "Rayy" . " rai";
     ?>
 
 </body>
