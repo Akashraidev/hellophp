bb13dd6e465d22be217d42144881d6973ad58f9f
akashraidev <akashrai3334@gmail.com>
2023-06-11 14:13:05 +0530

    function

---
+++ phpbasics/function.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    function writeMsg()
+    {
+        echo "Hello world!";
+    }
+
+    writeMsg();
+    writeMsg();
+    writeMsg();
+
+    echo "<br>";
+    // Function Arguments
+    function familyName($fname)
+    {
+        echo "$fname Refsnes.<br>";
+    }
+
+    familyName("A");
+    familyName("H");
+    familyName("S");
+    familyName("K");
+    familyName("B");
+
+
+    echo "<br>";
+    //  function with two arguments
+    
+    function functionwithtwoarguments($fname, $year)
+    {
+        echo "$fname Refsnes. Born in $year <br>";
+    }
+
+    functionwithtwoarguments("A", "1975");
+    functionwithtwoarguments("S", "1978");
+    functionwithtwoarguments("K", "1983");
+
+    ?>
+
+</body>
+
+</html>
