0e9861be4b861201b15e4af92642601e7c227124
akashraidev <akashrai3334@gmail.com>
2023-06-12 13:51:11 +0530

    GlobalVariable

--- phpbasics/array.php
+++ phpbasics/array.php
@@ -16,26 +16,15 @@
     for ($x = 0; $x < $arrlength; $x++) {
         echo $cars[$x];
         echo "<br>";
-
-
-        // PHP Associative Arrays
-        $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
-        echo "Peter is " . $age['Peter'] . " years old.";
-
-        ?>
-
-    </body>
-
-    </html>
-
-    <!-- <!DOCTYPE html>
-<html>
-<body>
-
-<?php
-
     }
+
+
+    // PHP Associative Arrays
+    $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+    echo "Peter is " . $age['Peter'] . " years old.";
+
     ?>
 
 </body>
-</html> -->
+
+</html>
